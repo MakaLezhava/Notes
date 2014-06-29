3490e1301b00bbca8ac372fbd5676cccac7e4321
MakaLezhava <ma_lezhava@cu.edu.ge>
2014-06-29 22:54:53 +0400

    database.php

---
+++ database.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once('config.php');
+
+function db_connect() {
+
+	$connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+
+	if (mysqli_connect_errno()) {
+   	 	printf("Connect failed: %s\n", mysqli_connect_error());
+    	exit();
+	}	
+
+	// mysqli_close($connection);
+
+	return $connection;
+}
+?>
+
